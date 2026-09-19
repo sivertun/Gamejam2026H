31f8e9bcfc8c4aee6b8e425ccaa2d344e9402ee1
Sander Eikemo Andreassen <180218942+seandreassen@users.noreply.github.com>
2026-09-19 12:27:40 +0200

    feat: rotate player towards mouse position

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -5,18 +5,46 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody rb;
+    private Camera mainCamera;
     [SerializeField] private float moveSpeed;
+
 
     [Header("Input Actions")]
     public InputActionReference Move;
+
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        mainCamera = Camera.main;
     }
 
     void FixedUpdate()
     {
         Vector3 m_Input = new Vector3(Move.action.ReadValue<Vector2>().x, 0, Move.action.ReadValue<Vector2>().y);
         rb.MovePosition(transform.position + m_Input * Time.fixedDeltaTime * moveSpeed);
+
+        rb.MoveRotation(FindLookRotation());
+    }
+
+    private Quaternion FindLookRotation()
+    {
+        if (Pointer.current != null)
+        {
+            Vector2 pointerScreenPos = Pointer.current.position.ReadValue();
+            Ray ray = mainCamera.ScreenPointToRay(pointerScreenPos);
+
+            Plane mouseInteractionPlane = new Plane(Vector3.up, transform.position);
+
+            if (mouseInteractionPlane.Raycast(ray, out float planeDistance))
+            {
+                Vector3 pointerWorldPos = ray.GetPoint(planeDistance);
+                Vector3 direction = pointerWorldPos - transform.position;
+                direction.y = 0;
+
+                return Quaternion.LookRotation(direction);
+            }
+        }
+
+        return Quaternion.identity;
     }
 }
