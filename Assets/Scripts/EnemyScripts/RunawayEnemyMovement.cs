07d28d05aba14ad85f24ac7ea8b52cd8352ad757
sivertun <sivertu03@gmail.com>
2026-09-19 22:15:08 +0200

    feat: add runaway enemy spawner

--- Assets/Scripts/EnemyScripts/RunawayEnemyMovement.cs
+++ Assets/Scripts/EnemyScripts/RunawayEnemyMovement.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 
-public class EnemyRunAwayFromLight : MonoBehaviour, IRunawayEnemy
+public class EnemyRunAwayFromLight : MonoBehaviour, IRunawayEnemy, IHasTarget, IHasVelocity
 {
     private Rigidbody rb;
 
